4c22d276fda99ec82de36b4158db99ebb5f42c24
tonehhh <tonywhitmarsh93@gmail.com>
2023-03-13 15:58:02 +0000

    implemented localstorage to maintain changes

--- script.js
+++ script.js
@@ -2,41 +2,76 @@ const inputText = document.querySelector('#text');
 const addButton = document.querySelector('.add-button');
 const list = document.querySelector('.container ul');
 
+// Render existing tasks from localStorage
+window.addEventListener('load', () => {
+  const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+  tasks.forEach(task => {
+    const myLi = document.createElement('li');
+    myLi.className = "task";
+    myLi.innerHTML = task.text;
+    list.appendChild(myLi);
 
-// add todo via plus button after typing down task input
+    const myInput = document.createElement('input');
+    myInput.type = "checkbox";
+    myInput.className = "completed";
+    myInput.checked = task.completed;
+    myLi.appendChild(myInput);
+
+    const mySpan = document.createElement('span');
+    mySpan.innerHTML = 'X';
+    myLi.appendChild(mySpan);
+  });
+});
+
+// Add new task to localStorage
 addButton.addEventListener('click', (e)=>{
-    if(inputText.value != ""){
-        e.preventDefault();
+  if(inputText.value != ""){
+    e.preventDefault();
 
-        //creates li element that contains task input
-        const myLi = document.createElement('li');
-        myLi.className = "task";
-        myLi.innerHTML = inputText.value;
-        list.appendChild(myLi);
+    const myLi = document.createElement('li');
+    myLi.className = "task";
+    myLi.innerHTML = inputText.value;
+    list.appendChild(myLi);
 
-        //creates input element in the form of a checkbox
-        const myInput = document.createElement('input');
-        myInput.type = "checkbox";
-        myInput.className = "completed"
-        myLi.appendChild(myInput);
+    const myInput = document.createElement('input');
+    myInput.type = "checkbox";
+    myInput.className = "completed";
+    myLi.appendChild(myInput);
 
-        //creates span element that's styled as a delete button
-        const mySpan = document.createElement('span');
-        mySpan.innerHTML = 'X';
-        myLi.appendChild(mySpan);
-    }
+    const mySpan = document.createElement('span');
+    mySpan.innerHTML = 'X';
+    myLi.appendChild(mySpan);
 
-    //assigned function of delete button to close task
-    const close = document.querySelectorAll('span');
-    for(let i=0; i<close.length; i++){
-        close[i].addEventListener('click', ()=>{
-            close[i].parentElement.style.opacity = 0;
-            setTimeout(() =>{
-                close[i].parentElement.style.display = "none";
-                close[i].parentElement.remove();
-            },500);
-            
-        })
-    }
-    inputText.value = "";
-})
+    const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+    tasks.push({ text: inputText.value, completed: false });
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  }
+
+  inputText.value = "";
+});
+
+// Update completion status in localStorage
+list.addEventListener('change', (e) => {
+  if (e.target.classList.contains('completed')) {
+    const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+    const index = [...list.children].indexOf(e.target.parentElement);
+    tasks[index].completed = e.target.checked;
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  }
+});
+
+// Remove task from localStorage
+list.addEventListener('click', (e) => {
+  if (e.target.tagName === 'SPAN') {
+    const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+    const index = [...list.children].indexOf(e.target.parentElement);
+    tasks.splice(index, 1);
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+
+    e.target.parentElement.style.opacity = 0;
+    setTimeout(() =>{
+      e.target.parentElement.style.display = "none";
+      e.target.parentElement.remove();
+    },500);
+  }
+});
