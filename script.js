c31d14dc3f2b5a04389a4dae5f8569b843035c6c
tonehhh <tonywhitmarsh93@gmail.com>
2023-03-20 15:21:08 +0000

    added comments for each function of code

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// select the input, add button and list elements
 const inputText = document.querySelector("#text");
 const addButton = document.querySelector(".add-task");
 const list = document.querySelector(".container ul");
@@ -6,11 +7,13 @@ const list = document.querySelector(".container ul");
 window.addEventListener("load", () => {
   const tasks = JSON.parse(localStorage.getItem("tasks")) || [];
   tasks.forEach((task) => {
+    // create a new list item for each task
     const listItem = document.createElement("li");
     listItem.className = "task";
     listItem.textContent = task.text;
     list.appendChild(listItem);
 
+    // add a checkbox and delete button to each list item
     const checkbox = document.createElement("input");
     checkbox.type = "checkbox";
     checkbox.className = "completed";
@@ -21,6 +24,7 @@ window.addEventListener("load", () => {
     deleteSpan.textContent = "X";
     listItem.appendChild(deleteSpan);
 
+    // set the decoration of the list item based on its completion status
     if (checkbox.checked) {
       listItem.style.textDecoration = "line-through";
     } else {
@@ -31,12 +35,15 @@ window.addEventListener("load", () => {
 
 // add new task to localStorage
 function addTaskToLocalStorage() {
+  // check if the input is not empty
   if (inputText.value.trim() !== "") {
+    // create a new list item for the task and add it to the list
     const listItem = document.createElement("li");
     listItem.className = "task";
     listItem.textContent = inputText.value.trim();
     list.appendChild(listItem);
 
+    // add a checkbox and delete button to the list item
     const checkbox = document.createElement("input");
     checkbox.type = "checkbox";
     checkbox.className = "completed";
@@ -46,11 +53,13 @@ function addTaskToLocalStorage() {
     deleteSpan.textContent = "X";
     listItem.appendChild(deleteSpan);
 
+    // add the new task to the tasks array in localStorage
     const tasks = JSON.parse(localStorage.getItem("tasks")) || [];
     tasks.push({ text: inputText.value.trim(), completed: false });
     localStorage.setItem("tasks", JSON.stringify(tasks));
   }
 
+  // clear the input field
   inputText.value = "";
 }
 
@@ -62,11 +71,13 @@ addButton.addEventListener("click", (e) => {
 // update completion status in localStorage
 list.addEventListener("change", (e) => {
   if (e.target.classList.contains("completed")) {
+    // update the completion status of the task in the tasks array in localStorage
     const tasks = JSON.parse(localStorage.getItem("tasks")) || [];
     const index = [...list.children].indexOf(e.target.parentElement);
     tasks[index].completed = e.target.checked;
     localStorage.setItem("tasks", JSON.stringify(tasks));
 
+    // set the decoration of the list item based on its completion status
     if (e.target.checked) {
       e.target.parentElement.style.textDecoration = "line-through";
     } else {
@@ -78,11 +89,13 @@ list.addEventListener("change", (e) => {
 // remove task from localStorage
 list.addEventListener("click", (e) => {
   if (e.target.tagName === "SPAN") {
+    // remove the task from the tasks array in localStorage
     const tasks = JSON.parse(localStorage.getItem("tasks")) || [];
     const index = [...list.children].indexOf(e.target.parentElement);
     tasks.splice(index, 1);
     localStorage.setItem("tasks", JSON.stringify(tasks));
 
+    // fade out and remove the list item
     e.target.parentElement.style.opacity = 0;
     setTimeout(() => {
       e.target.parentElement.style.display = "none";
